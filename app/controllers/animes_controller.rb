4e6c09b912d00c3643aa7030399cbf1f2f665909
matthewjstahnke <matthewjstahnke@gmail.com>
2021-01-11 22:09:02 -0600

    CRUD Completed

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -11,7 +11,6 @@ class AnimesController < ApplicationController
     post '/animes' do
         anime = current_user.animes.build(params[:anime])
         
-
         if anime.save
             redirect '/animes'
         else
@@ -19,4 +18,46 @@ class AnimesController < ApplicationController
         end
     end
 
+    get '/animes/:id' do
+        find_anime
+        redirect_if_not_exist
+        erb :'animes/show'
+    end
+
+    get '/animes/:id/edit' do
+        find_anime
+        redirect_if_not_exist 
+        redirect_if_not_owner
+        erb :'animes/edit'
+    end
+
+    patch '/animes/:id' do
+        find_anime
+        if @anime.update(params[:anime])
+            redirect "/animes/#{@anime.id}"
+        else
+            redirect "/animes/new"
+        end
+    end
+
+    delete '/animes/:id' do
+        find_anime
+        redirect_if_not_exist
+        @anime.destroy
+        redirect "/animes"
+    end
+
+    private
+
+        def find_anime
+            @anime = Anime.find_by_id(params[:id])
+        end
+
+        def redirect_if_not_owner
+            redirect "/animes" unless @anime.user == current_user
+        end
+      
+        def redirect_if_not_exist
+            redirect "/animes" unless @anime
+        end
 end
